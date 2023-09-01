a096fb736f0bc4b5b48be3f4402bb909989bd757
jenea130 <jenea130@mail.ru>
2023-09-01 13:50:09 +0300

    video: finished

--- src/assets/js/src/main.js
+++ src/assets/js/src/main.js
@@ -1,6 +1,14 @@
 import streetMap from "./modules/streetMap";
+import videoPlay from "./modules/video/videoPlay";
 
 window.addEventListener("DOMContentLoaded", function () {
+
+    const video = document.querySelector('.video');
+	
+	if (video) {
+		videoPlay();
+	}
+
 	console.log("some thing for me");
 	streetMap();
 });
