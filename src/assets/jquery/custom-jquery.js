95c20f11a0d49a7c9872b1cea8b042bd7d559e0d
jenea130 <jenea130@mail.ru>
2023-09-18 17:08:49 +0300

    upd: gallery mobile

--- src/assets/jquery/custom-jquery.js
+++ src/assets/jquery/custom-jquery.js
@@ -1,21 +1,35 @@
 $(document).ready(function () {
-    const slider = $(".gallery__slider")
+	const slider = $(".gallery__slider");
 
-    if (slider) {
-        gallerySlider()
-    }
+	if (slider) {
+		gallerySlider();
+	}
 
-    function gallerySlider() {
-        slider.slick({
-            infinite: true,
-            slidesToShow: 3,
-            slidesToScroll: 1,
-            speed: 1000,
-            arrows: false,
-            dots: false,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            easing: 'ease',
-        })
-    }
+	function gallerySlider() {
+		slider.slick({
+			infinite: true,
+			slidesToShow: 3,
+			slidesToScroll: 1,
+			speed: 1000,
+			arrows: false,
+			dots: false,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			easing: "ease",
+			responsive: [
+				{
+					breakpoint: 992,
+					settings: {
+						slidesToShow: 2,
+					},
+				},
+				{
+					breakpoint: 576,
+					settings: {
+						slidesToShow: 1,
+					},
+				},
+			],
+		});
+	}
 });
